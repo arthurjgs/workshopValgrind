7c7f79f2bfe714cb27a2376076f14d8c10dc180e
Virgile <virgile.agnel@epitech.eu>
2021-02-11 16:47:29 +0100

    [add] exo4

---
+++ ex04/main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct linked_s {
+    int data;
+    struct linked_s *next;
+} linked_t;
+
+static int add_new_elem(int i, linked_t **head)
+{
+    linked_t *new = malloc(sizeof(linked_t));
+    linked_t *tmp = *head;
+
+    if (new == NULL)
+        return (-1);
+    new->data = i;
+    new->next = NULL;
+    if (*head == NULL) {
+        *head = new;
+        return (0);
+    }
+    for(; tmp->next != NULL; tmp = tmp->next);
+    tmp->next = new;
+    return (0);
+}
+
+int main(void)
+{
+    linked_t *head;
+    int i = 0;
+
+    if (add_new_elem(i, &head) == -1)
+        return (84);
+    if (add_new_elem(1, &head) == -1)
+        return (84);
+    if (add_new_elem(2, &head) == -1)
+        return (84);
+    for (linked_t *tmp = head; tmp != NULL; tmp = tmp->next)
+        printf("%d\n", tmp->data);
+    free(head);
+    return (0);
+}
